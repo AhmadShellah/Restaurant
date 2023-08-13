9510c7236912e7b1570e6cfc906ebd706b6982b9
Yousef <yousefhs2023@gmail.com>
2023-08-13 13:37:48 +0300

    drinks and foods

---
+++ Service/DrinkService.cs
@@ -0,0 +1,17 @@
+﻿using Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    public class DrinkService : IDrinkService
+    {
+        public List<Class1.DrinksDto> GetDrinks()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
