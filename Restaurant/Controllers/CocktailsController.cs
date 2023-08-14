8fff6ea8ef2682817227e998102475b217d162e3
Yousef <yousefhs2023@gmail.com>
2023-08-14 11:07:15 +0300

    done

---
+++ Restaurant/Controllers/CocktailsController.cs
@@ -0,0 +1,23 @@
+﻿using Contracts;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Restaurant.Controllers
+{
+    public class CocktailsController : ICocktailService
+    {
+        private readonly ICocktailService _cocktailService;
+
+        public CocktailsController(ICocktailService cocktailService)
+        {
+            _cocktailService = cocktailService;
+        }
+
+        [HttpGet]
+        [Route("api/Cocktails/GetCocktails")]
+        public List<CocktailDto> GetCocktails()
+        {
+            return _cocktailService.GetCocktails();
+        }
+    }
+}
