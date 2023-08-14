34cb918763483d0a92c53edfde9c03583141ad90
malaaknasser <malaknasser650@gmail.com>
2023-08-14 13:05:50 +0300

    Deserts

---
+++ Restaurant/Controllers/DesertsController.cs
@@ -0,0 +1,28 @@
+﻿using Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Restaurant.Controllers
+{
+    [ApiController]
+    public class DesertsController : ControllerBase, IDesertsService
+    {
+        private readonly IDesertsService _desertService;
+        public DesertsController(IDesertsService DesertsService)
+        {
+            _desertService = DesertsService;
+        }
+
+        [HttpGet]
+        [Route("api/Deserts/GetDeserts")]
+        public List<DesertsDto> GetDeserts()
+        {
+            var Des = _desertService.GetDeserts();
+
+            return Des;
+        }
+
+
+
+
+    }
+}
