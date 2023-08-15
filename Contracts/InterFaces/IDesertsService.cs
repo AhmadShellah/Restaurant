d31b4b514672df6b2b21dc690a712fdef31eec80
malaaknasser <malaknasser650@gmail.com>
2023-08-15 15:59:13 +0300

    Create and Update Deserts

---
+++ Contracts/InterFaces/IDesertsService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Contracts.CreateObject;
+using Contracts.Dtos;
+using Contracts.UpdateObject;
+
+namespace Contracts.InterFaces
+{
+    public interface IDesertsService
+    {
+        public List<DesertsDto> GetDeserts();
+        public List<DesertsDto> CreateDeserts(CreateDesertsDto inputFromUser);
+        public List<DesertsDto> UpdateDeserts(UpdateDesertsDto inputFromUser);
+
+
+
+    }
+}
--- Restaurant/Program.cs
+++ Restaurant/Program.cs
@@ -1,6 +1,3 @@
-
-
-using Contracts;
 using Contracts.InterFaces;
 using Service;
 
